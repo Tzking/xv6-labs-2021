39edb6af37ab61318145e4fb2ca1cc6f71bb0c20
Tarryzking <2199749072@qq.com>
2024-05-27 09:18:52 +0800

    finish lab traps

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -93,6 +93,8 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
+extern uint64 sys_sigalarm(void);
+extern uint64 sys_sigreturn(void);
 
 static uint64 (*syscalls[])(void) = {
     [SYS_fork] sys_fork,
@@ -116,12 +118,8 @@ static uint64 (*syscalls[])(void) = {
     [SYS_link] sys_link,
     [SYS_mkdir] sys_mkdir,
     [SYS_close] sys_close,
-#ifdef LAB_NET
-    [SYS_connect] sys_connect,
-#endif
-#ifdef LAB_PGTBL
-    [SYS_pgaccess] sys_pgaccess,
-#endif
+    [SYS_sigalarm] sys_sigalarm,
+    [SYS_sigreturn] sys_sigreturn,
 };
 
 void syscall(void) {
