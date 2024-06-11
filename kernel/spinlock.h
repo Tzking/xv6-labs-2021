fa05b014e6b484ab0f044a57aacfda67817046ff
Tarryzking <2199749072@qq.com>
2024-06-11 10:27:55 +0800

    finsh thread

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,9 +1,23 @@
+#ifndef _SPINLOCK_H_
+#define _SPINLOCK_H_
+
+#include "types.h"
+
 // Mutual exclusion lock.
 struct spinlock {
-  uint locked;       // Is the lock held?
+    uint locked; // Is the lock held?
 
-  // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
+    // For debugging:
+    char *name;      // Name of lock.
+    struct cpu *cpu; // The cpu holding the lock.
 };
 
+// spinlock.c
+void acquire(struct spinlock *);
+int holding(struct spinlock *);
+void initlock(struct spinlock *, char *);
+void release(struct spinlock *);
+void push_off(void);
+void pop_off(void);
+
+#endif // _SPINLOCK_H_
